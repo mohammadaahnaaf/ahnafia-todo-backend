853887b7e71ef09cbd50ba551a28ffd79fcd642a
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-05-27 12:23:35 +0600

    app initialized

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const routes = require('./routes/ToDoRoutes')
+
+require('dotenv').config()
+const cors = require("cors");
+
+const app = express()
+const PORT = process.env.port || 5000
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URL).
+    then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log('error: ' + err))
+
+app.use(routes)
+app.listen(PORT, () => console.log(`Server is Listening Port: ${PORT}`))

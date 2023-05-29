2be924449159c7039834e8947fd1ec25db9061c7
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-05-29 12:10:07 +0600

    status upadate

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -1,16 +1,18 @@
 const ToDoModel = require("../models/ToDoModel");
 
+//get all data
 module.exports.getToDo = async (req, res) => {
     const todo = await ToDoModel.find();
     res.send(todo);
 }
 
+// create data 
 module.exports.saveToDo = (req, res) => {
-    const { text } = req.body;
+    const { text, status } = req.body;
 
     ToDoModel
-        .create({ text })
-        .then((data) =>{ 
+        .create({ text: text, status: false })
+        .then((data) => {
             console.log("Added Successfully...")
             console.log(data)
             res.send(data)
@@ -18,22 +20,24 @@ module.exports.saveToDo = (req, res) => {
         .catch((err) => console.log(err));
 }
 
+//delete data
 module.exports.deleteToDo = (req, res) => {
     const { _id } = req.body;
 
-    console.log('id ---> ', _id);
+    console.log('id: ', _id);
 
     ToDoModel
         .findByIdAndDelete(_id)
-        .then(() => res.set(201).send("Deleted Successfully..."))
+        .then(() => res.set(201).send("Deleted Successfully"))
         .catch((err) => console.log(err));
 }
 
+// edit data 
 module.exports.updateToDo = (req, res) => {
-    const { _id, text } = req.body;
+    const { _id, text, status } = req.body;
 
     ToDoModel
-        .findByIdAndUpdate(_id, { text })
-        .then(() => res.set(201).send("Updated Successfully..."))
+        .findByIdAndUpdate(_id, { text: text, status: status })
+        .then(() => res.set(201).send("Updated Successfully"))
         .catch((err) => console.log(err));
 }
--- models/ToDoModel.js
+++ models/ToDoModel.js
@@ -4,7 +4,19 @@ const todoSchema = new mongoose.Schema({
     text: {
         type: String,
         required: true
+    },
+    status: {
+        type: Boolean,
+        required: false
     }
 });
 
-module.exports = mongoose.model('ToDo', todoSchema);
+const yourSchemaName = new mongoose.Schema({
+    text: String,
+    status: Boolean
+  });
+
+module.exports = mongoose.model('ToDo', yourSchemaName);
+
+
+
